91791f5dbc6c74895ab407c7ad262ed64de8c0ee
lauro <lauropimentel12@gmail.com>
2023-11-29 21:28:20 -0300

    debug

--- login.php
+++ login.php
@@ -1,4 +1,7 @@
 <?php
+  ini_set('display_errors', '1');
+  ini_set('display_startup_errors', '1');
+  error_reporting(E_ALL);
     include('conexao.php');
 
     if (isset($_POST['email']) && isset($_POST['password'])) {
@@ -30,7 +33,7 @@
                 $_SESSION['name'] = $usuario['nome'];
 
                 header("Location: index.php");
-                exit();
+                exit;
             } 
             else {
                 echo '<div class="alert alert-danger" role="alert">
